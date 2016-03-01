9c62980a2cde032278f11049c2881e664c9641b5
Shakeh <Shakeh.E.Khudikyan@jpl.nasa.gov>
2016-03-01 13:51:50 -0800

    Update object with proper observation number and candidate png name

--- parse_cands.py
+++ parse_cands.py
@@ -16,6 +16,12 @@ with open(file) as pkl:
   state = pickle.load(pkl)
   cands = pickle.load(pkl)
 
+# print statec
+print state
+fileroot = state['fileroot']
+obs = fileroot.split('_')[0]
+print obs
+
 # Generate dumy dates
 alldata = []
 time = datetime.now()
@@ -23,7 +29,7 @@ date = time;
 for key in cands:
   data = {}
   date = date + timedelta(hours=1)
-  data['obs'] = '14A-425'
+  data['obs'] = obs
   data['@timestamp'] = date.isoformat()+'Z'
   data['scan'] = key[0]
   data['segment'] = key[1]
@@ -32,16 +38,21 @@ for key in cands:
   data['dt'] = key[4]
   value = cands[key]
   data['snr'] = value[0]
+  
+  # candidate png example -- cands_15B-305_sb31668497_1.57411.26645269676_sc38-seg0-i1082-dm96-dt0.png
+  cand_png = 'cands_' + fileroot + '_sc' + str(data['scan']) + '-seg' + str(data['segment']) + '-i' + str(data['integration'])+ '-dm' + str(data['dm']) + '-dt' + str(data['dt']) + '.png';
+  data['candidate_png'] = cand_png
+  # print cand_png
+  
   idobj = {}
   idobj['_id'] = data['obs'] + '_' + str(data['scan']) + '_' + str(data['segment'])+ '_' + str(data['integration'])
-  # print data
 
   alldata.append({"index":idobj})
   alldata.append(data)
 
 jsonStr = json.dumps(alldata,separators=(',', ':'))
 cleanjson = jsonStr.replace('}},','}}\n').replace('},','}\n').replace(']','').replace('[','')
-print cleanjson
+# print cleanjson
 url = 'http://localhost:9200/veryfast/cands/_bulk?'
 r = requests.post(url, data=cleanjson)
 print r
